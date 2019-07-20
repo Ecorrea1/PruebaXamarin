adad466e2078b0241f1212cc1a50a7ba95c29e1e
EMMANUEL BERNABE CORREA CEBALLOS <emmanuel.correa@inacapmail.cl>
2019-07-20 00:36:44 -0400

    Agregar archivos de proyecto.

---
+++ PruebaXamarin/PruebaXamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PruebaXamarin.Models;
+
+namespace PruebaXamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
